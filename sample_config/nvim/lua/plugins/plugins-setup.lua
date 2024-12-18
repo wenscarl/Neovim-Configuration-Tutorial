bca7c3f0267a0cf7af08051c51f5f56dca953ea0
Shu Wang <shuw@nvidia.com>
2024-12-17 22:18:33 -0600

    update telescope

--- sample_config/nvim/lua/plugins/plugins-setup.lua
+++ sample_config/nvim/lua/plugins/plugins-setup.lua
@@ -58,7 +58,7 @@ return require('packer').startup(function(use)
   use "lewis6991/gitsigns.nvim" -- 左则git提示
 
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',  -- 文件检索
+    'nvim-telescope/telescope.nvim', tag = '0.1.4',  -- 文件检索
     requires = { {'nvim-lua/plenary.nvim'} }
   }
 
